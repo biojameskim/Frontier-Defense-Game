629603ed5b00c4ce5ce136ea3f27d34130d053f7
dliu42 <dtl54@cornell.edu>
2022-10-16 01:06:14 -0400

    Rearrange all the code
    -I had to comment out everything to make sure it worked, but I rearranged all the files.

---
+++ plantsvzombies/src/board.ml
@@ -0,0 +1,46 @@
+(* open Characters
+
+
+module Game = struct
+  let n_cols = 10
+  let n_rows = 5
+  type cell = { 
+    plant : plant option;
+    zombie : zombie option;
+    pea : pea option;
+    lawnmower : lawnmower option
+    }
+
+(** cells represents the cells (grid block) in that row
+  zombies represents the list of zombies in that row
+  plants represents the list of plants in that row
+  peas represents the list of peas in that row 
+*)
+  type row = {
+  cells : cell list;
+  zombies : zombie list;
+  plants : plant list;
+  peas : pea list;
+  lawnmower : lawnmower
+  }
+
+  type t = {
+  rows : row list
+  }
+
+  (* we want to intialitize the grid with an empty list of zombies, plants, and peas 
+     because at the beginning, there are no zombies, plants, nor peas in the row. *)
+  let init_row () = {
+  cells = List.init n_cols (fun _ -> { plant = None });
+  zombies = []
+  plants = []
+  peas = []
+  }
+  
+  let init () = {
+  rows = List.init n_rows (fun _ -> init_row ())
+  }
+end
+  
+let game = Game.init ()
+  *)
