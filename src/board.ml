2f5f21bd1b87bd33a361a89b6a6564445c992f1a
James Kim <biojameskim2002@gmail.com>
2022-11-30 21:09:19 -0500

    Initialize Mutable Lawnmower

--- src/board.ml
+++ src/board.ml
@@ -2,8 +2,8 @@ open Characters
 
 (* model, defines t (the whole board), init makes the board filled with random
    stuff*)
-let n_cols = 10
-let n_rows = 5
+let num_cols = 10
+let num_rows = 5
 
 type cell = { mutable plant : plant option }
 
@@ -11,7 +11,7 @@ type row = {
   cells : cell list;
   mutable zombies : zombie list;
   mutable peas : pea list;
-  lawnmower : lawnmower option;
+  mutable lawnmower : lawnmower option;
 }
 (** cells represents the cells (grid block) in that row zombies represents the
     list of zombies in that row plants represents the list of plants in that row
@@ -19,22 +19,27 @@ type row = {
 
 type t = { rows : row list }
 
-(* we want to intialitize the grid with an empty list of zombies, plants, and
-   peas because at the beginning, there are no zombies, plants, nor peas in the
-   row. *)
-let init_row row =
+(** [init_row] intialitizes a row in the grid with an empty list of zombies,
+    plants, and peas, and an initial lawnmower. *)
+let init_row row_id =
   {
-    cells = List.init n_cols (fun _ -> { plant = None });
+    cells = List.init num_cols (fun _ -> { plant = None });
     zombies = [];
     peas = [];
     lawnmower =
-      Some { speed = 0; damage = 0; location = (10, row * 100); row = 0 };
+      Some
+        {
+          speed = 0;
+          damage = 10000;
+          location = (145, row_id * 144);
+          row = row_id;
+        };
   }
 
 (* calls the init_row function n_rows time *)
-let init () = { rows = List.init n_rows init_row }
+let init () = { rows = List.init num_rows init_row }
 
-(* [spawn_zombie zombie_type t] spawns a zombie *)
+(** [spawn_zombie zombie_type t] spawns a zombie *)
 let spawn_zombie (zombie_type : zombie_type) (t : t) =
   let row_id = Random.int 5 in
   let row = row_id |> List.nth t.rows in
@@ -44,7 +49,7 @@ let spawn_zombie (zombie_type : zombie_type) (t : t) =
       hp = 10;
       damage = 1;
       location = (1280, row_id * 144);
-      speed = 1;
+      speed = 10;
       frame = 0;
     }
   in
