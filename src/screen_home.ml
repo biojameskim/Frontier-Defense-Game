502faed1f65f882b1be1d6b93d53156ae52450b4
James Kim <biojameskim2002@gmail.com>
2022-12-11 16:36:40 -0500

    Updated Game Name and Screen Designs
    - Updated message for game lost screen
    - Updated name of game to "Frontier Defense" and added designs
    - Added navigation to home screen and pause screen
    - Updated design and spacing of tutorial screens
    - Updated state to include images for a white background

--- src/screen_home.ml
+++ src/screen_home.ml
@@ -3,17 +3,37 @@ open Image_graphics
 open Png
 open Images
 
-let draw st ev =
-  let on_play st = st |> State.change_screen Screen.TutorialScreen1 in
+let draw (st : State.t) ev =
+  let on_play st = st |> State.change_screen Screen.PlayScreen in
+  let on_tutorial st = st |> State.change_screen Screen.TutorialScreen1 in
   let on_quit _ = exit 0 in
-  draw_string_p
-    (CenterPlace (1280 / 2, 500))
-    ~size:GiantText "Plants vs. Zombies";
+  draw_string_p (CenterPlace (1280 / 2, 570)) ~size:GiantText "Frontier Defense";
+  (* row 1 *)
+  draw_image_with_placement st.images.rifle_soldier_tutorial 89 100
+    (CenterPlace (430, 410));
+  draw_image_with_placement st.images.regular_bullet_tutorial 89 100
+    (CenterPlace (550, 482));
+  draw_image_with_placement st.images.regular_enemy_tutorial 89 100
+    (CenterPlace (860, 410));
+  (* row 2 *)
+  draw_image_with_placement st.images.rocket_launcher_soldier_tutorial 89 100
+    (CenterPlace (340, 310));
+  draw_image_with_placement st.images.rocket_bullet_tutorial 89 100
+    (CenterPlace (460, 382));
+  draw_image_with_placement st.images.shield_soldier_tutorial 89 100
+    (CenterPlace (520, 310));
+  draw_image_with_placement st.images.shield_enemy_1_tutorial 89 100
+    (CenterPlace (770, 310));
+  draw_image_with_placement st.images.shield_enemy_2_tutorial 89 100
+    (CenterPlace (950, 310));
   Events.add_clickable
-    (draw_button (placed_box (CenterPlace (1280 / 2, 195)) 130 50) "Play")
+    (draw_button (placed_box (CenterPlace (645, 170)) 155 50) "Tutorial")
+    on_tutorial ev;
+  Events.add_clickable
+    (draw_button (placed_box (CenterPlace (570, 100)) 130 50) "Play")
     on_play ev;
   Events.add_clickable
-    (draw_button (placed_box (CenterPlace (1280 / 2, 135)) 130 50) "Quit")
+    (draw_button (placed_box (CenterPlace (720, 100)) 130 50) "Quit")
     on_quit ev
 
 let tick st = st
