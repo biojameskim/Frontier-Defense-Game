abccd0b708befa20fffd8b3047c51e7d24c95359
James Kim <biojameskim2002@gmail.com>
2022-12-12 03:49:10 -0500

    Update styling of win screen

--- src/screen_end_win.ml
+++ src/screen_end_win.ml
@@ -3,11 +3,12 @@ open Gui_util
 let draw (st : State.t) ev =
   let on_home st = State.init () in
   let on_quit _ = exit 0 in
+  draw_string_p (CenterPlace (1280 / 2, 520)) ~size:GiantText "Congratulations!";
   draw_string_p
-    (CenterPlace (1280 / 2, 480))
-    ~size:BigText "Congrats! You have successfully defended off the enemies";
+    (CenterPlace (1280 / 2, 430))
+    ~size:BigText "The frontier lives on!";
   Events.add_clickable
-    (draw_button (placed_box (CenterPlace (1280 / 2, 350)) 130 50) "Restart")
+    (draw_button (placed_box (CenterPlace (1280 / 2, 320)) 200 50) "Play Again")
     (fun st ->
       let new_game = State.init () in
       new_game.screen <- PlayScreen;
