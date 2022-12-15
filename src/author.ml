fa59db7e5dd3b5703bc8a844a0300f1e8037dad3
James Kim <biojameskim2002@gmail.com>
2022-12-14 20:08:49 -0800

    Add author.ml and my reflection

---
+++ src/author.ml
@@ -0,0 +1,13 @@
+let james_reflection =
+  "Coming into this project, I was pretty overwhelmed at the idea of having to \
+   create a large scale project from scratch. After scrapping multiple ideas, \
+   from a RDBMS to a Dungeon Crawler game, we finally ended up settling on \
+   implementing a clone of the classic game Plants vs. Zombies. It was a lot \
+   of fun learning how to work with a GUI and once we finally understood what \
+   we were doing, I found myself wanting to add more and more features to our \
+   game. I was mostly in charge of the lawnmower (horse) logic and drawing the \
+   characters used in our game, and adding support for those PNG files in our \
+   game using camlimages. Finding support for integrating different packages \
+   from Graphics to Camlimages was such a pain because nothing in OCaml is \
+   well documented. Nevertheless, this was a great learning experience and I'm \
+   proud of how far my group has come. 12/14/2022."
---
+++ src/author.mli
@@ -0,0 +1,9 @@
+(** CS 3110 Fall 2022 Final Project - Frontier Defense Game
+
+    @author Derek Liu (DTL54)
+    @author Suhao Jeffrey Huang (SJH292)
+    @author Eric Zhang (ESZ8)
+    @author James Kim (JJK297) *)
+
+val james_reflection : string
+(** [james_reflection] is James's overall reflection of this final project. *)
